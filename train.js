0aed476ccefe13c5e44375170077c0e81a224604
Ronny2101 <dilshoddaminov2121@gmail.com>
2025-03-13 15:07:57 +0900

    BRR:.installing...

--- train.js
+++ train.js
@@ -1,8 +1,8 @@
 function getHighestIndex(arr) {
-    let maxIdx = 0;
+    let b = 0;
     for (let i = 1; i < arr.length; i++) 
-        if (arr[i] > arr[maxIdx]) maxIdx = i;
-    return maxIdx;
+        if (arr[i] > arr[b]) b=i ;
+    return b;
 }
 
 console.log(getHighestIndex([5, 21, 12, 21, 8]));
