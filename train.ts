4939a9ba35525dc3a0eee2abed3019afe848ca24
Ronny2101 <dilshoddaminov2121@gmail.com>
2025-03-15 18:18:54 +0900

    fix:express integrations

--- train.ts
+++ train.ts
@@ -3,15 +3,17 @@
 //     for (let i = 1; i < arr.length; i++) 
 //         if (arr[i] > arr[highIndex]) highIndex=i ;
 //     return highIndex;
-// }
+// }s
 
 // console.log(getHighestIndex([5, 21, 12, 21, 8]));
 
 
 //Task H
-function getPositive(arr: number[]): string {
-    return arr.filter((num: number) => num > 0).join('');
-}
 
+//typescriptda argument type ko'rsatilmasa error beradi
+
+function getPositive(arr: number[]): string {
+    return arr.map((num: number) => (num > 0 ? num : '')).join('');
+}
 
 console.log(getPositive([1, -4, 2]));
