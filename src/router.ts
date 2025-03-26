99dad9e58c4cc4c51ae6b1b63125ad87b93f594b
Ronny2101 <dilshoddaminov2121@gmail.com>
2025-03-26 21:42:02 +0900

    fix: organize spa & ssr business logic

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import express from "express";
-import memberController from "./controllers/member.controller";
 const router = express.Router();
- 
+import memberController from "./controllers/member.controller";
+
+router.post("/login",memberController.login);
+router.post("/signup",memberController.signup);
+
 
 export default router;
