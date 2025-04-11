e4bf008976c73ad25343ad15837c262f781cb30c
Ronny2101 <dilshoddaminov2121@gmail.com>
2025-04-11 18:45:54 +0900

    fix: modify products page based on products data

--- train.ts
+++ train.ts
@@ -133,16 +133,16 @@ Project standards
 
 //Task O
 
-function calculateSumOfNumbers(arr: any[]) {
-   let sum: number = 0;
+// function calculateSumOfNumbers(arr: any[]) {
+//    let sum: number = 0;
 
-   for(const item of arr) {
-      if(typeof item === "number") {
-         sum += item
-      }
-   }
-   return sum;
-}
+//    for(const item of arr) {
+//       if(typeof item === "number") {
+//          sum += item
+//       }
+//    }
+//    return sum;
+// }
 
 
-console.log(calculateSumOfNumbers([10, "10", {son: 10}, true,35]));
+// console.log(calculateSumOfNumbers([10, "10", {son: 10}, true,35]));
